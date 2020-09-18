f65324920a5de8059319500235f8be2e2ddcd09e
BenLooper <benlooper08@gmail.com>
2020-09-18 13:00:21 -0500

    made user show page, divided controllers based on models, linked existing pages with <a> tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,6 @@ class ApplicationController < Sinatra::Base
         erb :index
     end 
 
-    get '/:id' do 
-        @post = Post.find(params[:id])
-        erb :show
-    end 
+   
 
 end 
---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,14 @@
+class PostController < Sinatra::Base
+
+    set :views, 'app/views/posts'
+    set :method_override, true
+
+    get '/posts/:id' do 
+        @post = Post.find(params[:id])
+        user_name = @post.user.name
+        @user = User.find_by name:user_name
+        erb :show
+    end 
+
+    
+end 
